8d8c2e1e917e42bb545b6de839dc466fc62d69c6
admin <admin@Bcalab4-m15>
2022-03-17 10:01:21 +0530

    commit cons and method

---
+++ lab3/src/Student.java
@@ -0,0 +1,19 @@
+package Lab3;
+
+public class Student {
+
+	String name;
+	Student(String name){
+		
+		this.name = name;
+	}
+	void display() {
+		System.out.println("Student name is "+ name);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method st
+		
+		System.out.println("Hello");
+	}
+
+}
